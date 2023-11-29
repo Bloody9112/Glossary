f794d4405988ff98eb3c85e278bf6a47caab7df2
Bloody9112 <57665482+Bloody9112@users.noreply.github.com>
2023-11-29 21:06:01 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 document.addEventListener('DOMContentLoaded', function () {
   function hideLoader() {
     document.querySelector('.loader-wrapper').style.display = 'none';
+    document.querySelector('.logo img').classList.add('animation-start');
+    document.querySelector('.header_left').classList.add('animation-start');
   }
-  
-  document.querySelector('.logo img').classList.add('animation-start');
-  document.querySelector('.header_left').classList.add('animation-start');
 
   setTimeout(hideLoader, 3800);
 });
