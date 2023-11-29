3eb5154c0530c4faf8066480d24613d362a7b8b9
Bloody9112 <57665482+Bloody9112@users.noreply.github.com>
2023-11-29 19:54:56 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,7 @@
+window.addEventListener('load', function() {
+    function hideLoader() {
+      document.querySelector('.loader-wrapper').style.display = 'none';
+    }
+    setTimeout(hideLoader, 3800);
+  });
+  
